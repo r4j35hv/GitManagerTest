31ab526a2af157c17fe4f3aaa164646c25739e21
FUD <rajeshdvaishnav@gmail.com>
2018-12-19 00:16:08 +0530

    first commit

---
+++ app/src/main/java/r4j35h/gitmanagmenttestrajesh/RepoDetailsActivity.java
@@ -0,0 +1,13 @@
+package r4j35h.gitmanagmenttestrajesh;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RepoDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repo_details);
+    }
+}
